0465086f31d1ca2b0708e1ae0af2d3653226dce1
Rahul Goyal <87524423+Goyalrahul1516@users.noreply.github.com>
2024-08-28 11:41:04 +0530

    Initial

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAm0LK6poFHw06ZrYaXzNXvZqv5D9sdMOo",
+    authDomain: "grasp-6b2ea.firebaseapp.com",
+    projectId: "grasp-6b2ea",
+    storageBucket: "grasp-6b2ea.appspot.com",
+    messagingSenderId: "185235518954",
+    appId: "1:185235518954:web:79a723cd10ee60fc212e93",
+    measurementId: "G-3HS7NJ34JB"
+};
+        
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, signInWithEmailAndPassword, db, collection, getDocs, signOut};
---
+++ script.js
@@ -0,0 +1,37 @@
+import { auth, signInWithEmailAndPassword, signOut} from './firebase.js'; // Import Firebase-related functions from the new file
+
+// Handle login form submission
+document.getElementById('login-form')?.addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    var email = document.getElementById('email')?.value;
+    var password = document.getElementById('password')?.value;
+    
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        alert('Login Successful');
+        window.location.href = "dashboard.html"; // Redirect to the new page
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert('Error: ' + errorMessage);
+    });
+});
+
+// Handle logout button click
+document.getElementById('logout-btn')?.addEventListener('click', () => {
+    signOut(auth).then(() => {
+        // Sign-out successful.
+        alert('Logged out successfully');
+        window.location.href = "index.html"; // Redirect to login page or any other page
+    }).catch((error) => {
+        // An error happened.
+        console.error('Logout Error:', error);
+        alert('Error logging out: ' + error.message);
+    });
+});
+
+
